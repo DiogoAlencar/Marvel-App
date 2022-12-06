4a3f84bdd02d624e1ee7fabc9f30bb84ba97e557
diogo <92116068+DiogoAlencar@users.noreply.github.com>
2022-12-05 22:54:16 -0300

    Navigation entre Fragments usando BottomNavigation e NavGraph

--- app/src/main/java/daniel/lop/io/marvelappstarter/ui/MainActivity.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package daniel.lop.io.marvelappstarter.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import daniel.lop.io.marvelappstarter.R
 import daniel.lop.io.marvelappstarter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,11 +12,25 @@ class MainActivity : AppCompatActivity() {
     // public key: 89a6951dda6eb99a004e7ae36cefc027
     // private key: 291adc521210e383d6fe3e685db2578aed414f4d
 
+    private lateinit var navHostFragment : NavHostFragment
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
+        initViews(binding)
+    }
+
+    private fun initViews(binding: ActivityMainBinding) {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener {}
+        }
     }
 }
